5a2abef3f9ea9ff74a088cb3f9f4faf339d147de
jenny7hi <33742243+jenny7hi@users.noreply.github.com>
2018-09-20 14:33:41 -0700

    responsive

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 $(document).ready(function(){
+
+    $('#header').css('max-height', $('#header-pic').height());
+    $( window ).resize(function() {
+        $('#header').css('max-height', $('#header-pic').height());
+    });
+
     $('.navbar-brand').on('click', function (e) {
         $('html, body').animate({
             scrollTop: $('body').offset().top
         }, 700);
     });
 
+    $navbarHeight = $('.navbar').height();
     $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - $('.navbar').height()
+            scrollTop: $($(this).attr('href')).offset().top - $navbarHeight
         }, 700);
     });
 });
